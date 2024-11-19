f30dda0adf5b1a6bcc09d0ad336ebc8796ed2e07
jayzilpilwar <jayzilpilwar@gmail.com>
2024-11-19 16:15:08 +0530

    remove extra table

--- sql_setup.sql
+++ sql_setup.sql
@@ -1,13 +1,6 @@
 CREATE database if not   EXISTS mcq_test;
 USE mcq_test;
 
--- Table to track tests
-CREATE TABLE tests (
-    test_id INT AUTO_INCREMENT PRIMARY KEY,
-    test_name VARCHAR(255),
-    total_questions INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 -- Table to track test sessions
 CREATE TABLE test_sessions (
